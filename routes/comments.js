dcd5da953cd094e3a3be29faf1f62b48898336f5
RomanFisc <romanfisc@gmail.com>
2024-07-10 15:44:15 -0400

    adding comments and getting comments

--- routes/comments.js
+++ routes/comments.js
@@ -8,4 +8,35 @@ router.get("/", (req, res) => {
   res.json(comments);
 });
 
+router.post("/", (req, res, next) => {
+  const { userId, postId, body } = req.body;
+  
+  if (!userId || !postId || !body) {
+    return next(error(400, "Insufficient Data"));
+  }
+  
+  const newComment = {
+    id: comments.length ? comments[comments.length - 1].id + 1 : 1,
+    userId,
+    postId,
+    body
+  };
+  
+  
+  comments.push(newComment);
+  
+  res.status(201).json(newComment);
+});
+
+router.get('/:id', (req, res, next) => {
+  const commentId = parseInt(req.params.id, 10);
+  const comment = comments.find(c => c.id === commentId);
+
+  if (!comment) {
+    return next(error(404, 'Comment Not Found'));
+  }
+
+  res.json(comment);
+});
+
 module.exports = router;
